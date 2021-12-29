58b4dad8a19f1f90b4f84b0ddfa51df25d496aa9
wanlihang <lihang_wan@outlook.com>
2021-12-29 20:06:58 +0800

    fix: 修复eduSystem模块后端配置问题

--- eduSystem/src/main/java/com/wlh/springcloud/EduSystemMain8888.java
+++ eduSystem/src/main/java/com/wlh/springcloud/EduSystemMain8888.java
@@ -1,11 +1,13 @@
 package com.wlh.springcloud;
 
 import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @Slf4j
 @SpringBootApplication
+@MapperScan("com.wlh.springcloud.dao")
 public class EduSystemMain8888 {
     public static void main(String[] args) {
         SpringApplication.run(EduSystemMain8888.class,args);
